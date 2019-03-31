949cf4a78e9ecce981290386c8563f2d1a3fd0cd
Aluisyo <suzyoeliasnyirenda@gmail.com>
2019-03-31 16:51:34 +0200

    explorer

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = 'https://node.aluisyo.network/api';
+var donationAddress = "";
+var blockTargetInterval = 120;
+var coinUnits = 1000000;
+var symbol = 'ANX';
+var refreshDelay = 5000;
+var networkStat = {
+ "anx": [
+	["coinsforhash.com/aluisyo", "https://coinsforhash.tk:8132"],
+    ["pool.aluisyo.network", "https://pool.aluisyo.network/api"]
+
+]
+};
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+        'api' => 'https://node.aluisyo.network/api',
+        'blockTargetInterval' => 120,
+        'coinUnits' => 1000000    
+);
+?>
